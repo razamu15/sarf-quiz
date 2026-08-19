234a2ae6399fa7fe0431297adc2e51186d704e9d
razamu15 <m.saad.raza1999@gmail.com>
2026-08-19 09:04:16 -0400

    i did naqis grammar objects and wrote some pseudo code for the conjugator.

--- web-prototype/js/conjugation/naqis-conjugator.js
+++ web-prototype/js/conjugation/naqis-conjugator.js
@@ -32,6 +32,56 @@ export const NaqisConjugator = {
 
   /** @returns {null} — scaffold. */
   conjugate(_spec, _slot) {
+    // so for naqis things are a little bit different, up until now, for all the verb types we have just
+    // been able to define stems and endings and things have always worked, specifically when trying to
+    // conjugate words in different moods such as nasb and jazm. Up untill now we could do that with only
+    // the endings. now that is not the case for naqis because the word itself (ie the stem) 
+    // changes based on the mood,
+
+    // So in order to tackle this we will handle those special cases imperically with code, instead of
+    // encoding it as data in our objects in the grammar file. 
+    if (spec.tense === 'mudari' && spec.mood === 'nasb') {
+      // theres 5 endings that are affected by harakaat changes when changing the mood of the verb.
+      // these ending are (he, she, I, we, you (masc)) is a fatha.
+      // the rest we change the mood by dropping the noon for both nasb and jazm.
+      // so for dropping the noon we will do it via the grammar object as we have done for all the other verb types,
+      // but the 5 endings we will do it here because they are dependant on our baabs and endings objects do not contain
+      // baab info (this is just a convention we have.)
+      if (spec.root.baab === 'au' || spec.root.baab === 'ai') {
+        // the five endings get a fatha on the last letter which is also the last radical of the root
+        // all the other seegahs proceed as normal, ie the noon gets dropped to show nasb
+      } else {
+        // baabs are aa and ia
+        // no fatha is added for mansub, so the ending for the 5 forms (he, she, I, we); mansub == marfu
+        // this makes sense because the AA sound is already there.
+      }
+    }
+
+    if (spec.tense === 'mudari' && spec.mood === 'jazm') {
+      // the ending for the 4 forms affected (he, she, I, we), the weak letter is always dropped
+      // all the other seegahs proceed as normal, ie the noon gets dropped to show jazm
+    }
+
+
+    // after the work is fully conjugated with the endings and everything, we call the letter swap function
+    // this is because we only want to do the letter swap on the original radicals of the work, and not on
+    // any of the letters of the endings. this seems contradictory but it works because all the madi and mudari
+    // endings do not end with a weak letter, except for the seegahs where there is no ending and we get to see
+    // the "raw endings" of the root word. Which is exactly the seegahs were the swapping logic applies
+    // the seegahs are 3ms, 3fs, 2ms, 1s, 1p
+    // also this makes majhool work because in the template we have hardcoded mudari majhool ending to be ya,
+    // and so this swap saves on the seegahs where it needs to be alif maqsura ى while keeping it simple because
+    // all the other forms use the ya. 
+    return naqisWeakLetterSwap(word)
+  },
+
+  // fully conjugated naqis word is given
+  naqisWeakLetterSwap(word) {
+    if (word.endsWith('و') || word.endsWith('ي')) {
+      // if the haraka before the word is a fatha, then the weak letter is written as ى (without dots under)
+      // if the haraka before the word is a dammah, then the weak letter is written as wow
+      // if the haraka before the word is a kasrah, then the weak letter is written as ي (with dots under)
+    }
     return null;
   },
 
--- web-prototype/js/grammar/naqis-grammar.js
+++ web-prototype/js/grammar/naqis-grammar.js
@@ -16,7 +16,111 @@ import { A } from './shared-grammar.js';
 import { SALIM_ENDINGS, SALIM_VERB_STEMS } from './salim-grammar.js';
 
 /** Stem templates, per form. Shape them for the nāqiṣ. */
-export const NAQIS_STEMS = {};
+export const NAQIS_STEMS = {
+
+  I: {
+    madi_malum: {
+      au: {
+        // TODO we need alif becuse 3rd m sing ending is empty string so we need the alif
+        // but also we cant put an alif here because itll bleed over to all the other seegah
+        // i think well need to implement the function that check the last letter and replaces 
+        // if needed based on the naqis rules cz itll be used by mudari as well. 
+        regular:  '1' + F + '2' + F + '3',
+        dropping: '1' + F + '2' + F
+      },
+      ai: {
+        regular:  '1' + F + '2' + F + '3',
+        dropping: '1' + F + '2' + F
+      },
+      aa: {
+        regular:  '1' + F + '2' + F + '3',
+        dropping: '1' + K + '3'
+      },
+      ia: {
+        // for ia naqis verbs, the only seegah that is different is 3rd masc plural,
+        // because of the kasrah on the ayn kalima, it makes the last weak letter ya pronounceable
+        // for all other seegahs. so only this baab strays from the information recorded in NAQIS_DROPPING_SLOTS_MADI
+        regular:  '1' + F + '2' + K + '3',
+        // another thing to note is that the ia means it should have dammah on the second radical, but
+        // because we drop the ya, the wow in the ending of the masculine plural would be preceeded by
+        // a karsah which is not allowed to it is changed into a dammah to correspond to the wow [NOTE A1]
+        thirdMascPlural: '1' + F + '2' + D
+      }
+    },
+    // now the madi majhul chart for naqis behaves exactly like the maroof ia baab because the majhul
+    // pattern puts a kasrah on the second radical, which is exactly the ia baab.
+    madi_majhul: {
+      // however madi majhul has one more quirk, which is that last weak letter is always a ya
+      regular:  '1' + F + '2' + K + 'ي',
+      // [NOTE A1]
+      thirdMascPlural: '1' + F + '2' + D
+    },
+
+    mudari_malum: {
+      au: {
+        // basically the reason for this structure is 2 fold. 1 is that these are the 3 forms, that
+        // drop the naqis weak letter. the reason for separating the femSingular and mascPlural is
+        // because the haraka on the second radical for those 2 seegahs is different across the baabs,
+        // and not consistent between them on the exact same table.
+        regular:  '1' + S + '2' + D + '3',
+        femSingular: '1' + S + '2' + K,
+        mascPlural: '1' + S + '2' + D 
+      },
+      ai: {
+        // now the thing that happened with the ia baab in the madi majhul, is the same thing that 
+        // happens for the ai baab in the mudari malum, basically it actually does drop its weak letter
+        // everywhere that is labelled in the NAQIS_DROPPING_SLOTS_MUDARI
+        regular:  '1' + S + '2' + K + '3',
+        // only applicable to one seegah, 2nd person fem singular
+        femSingular: '1' + S + '2' + K,
+        // BUT, the because of the kasrah rule we talked about in [NOTE A1], the 3rd & 2nd masc plural
+        // needs a different stem because the ayn kalima needs to take a dammah instead of a kasrah. 
+        mascPlural: '1' + S + '2' + D
+      },
+      // just to point the obvious but one of the reasons for us to make the affix.haraka to blank
+      // for the NAQIS_DROPPING_SLOTS_MUDARI baabs was so that we can give a fatha here at the end of
+      // the femSingular and mascPlural stems bcz if you look at the salim grammar file, they would
+      // be getting a kasrah and a dammah respectively
+      aa: {
+        regular:  '1' + S + '2' + D + '3',
+        femSingular: '1' + S + '2' + F,
+        mascPlural: '1' + S + '2' + F 
+      },
+      ia: {
+        regular:  '1' + S + '2' + D + '3',
+        femSingular: '1' + S + '2' + F,
+        mascPlural: '1' + S + '2' + F 
+      }
+    },
+    // because all the naqis mudari majhool are on the same pattern, they look very similar to the mudari malum verbs
+    // of the ia or aa baabs, because they all have a fatha on the ayn kalima. 
+    mudari_majhul: {
+      regular:  '1' + S + '2' + D + 'ي',
+      femSingular: '1' + S + '2' + F,
+      mascPlural: '1' + S + '2' + F 
+    },
+  },
+  II: {},
+  III: {},
+  IV: {},
+  V: {},
+  VI: {},
+  VII: {},
+  VIII: {},
+  IX: {},
+  X: {}
+};
+
+// naqis has 3 specific seegah that drop the weak letter that do not correspond to a singular group.
+// they drop them because ow you would have 2 weak letter back to back
+const NAQIS_DROPPING_SLOTS_MADI = [
+  '3mp', '3fs', '3fd'
+];
+
+const NAQIS_DROPPING_SLOTS_MUDARI = [
+  '3mp', '2mp' , '2fs'
+];
+
 
 /**
  * Ending tables — the interesting half of this verb type. The weak letter IS
@@ -24,7 +128,40 @@ export const NAQIS_STEMS = {};
  * (رَمَوْا، يَرْمُونَ، تَرْمِينَ). Write the ones that change out in full;
  * SALIM_ENDINGS is imported for the rows that genuinely don't.
  */
-export const NAQIS_ENDINGS = {};
+export const NAQIS_ENDINGS = {
+  madi: SALIM_ENDINGS.madi,
+
+  // the thing with naqis is that we cannot just use the salim endings because for certain baabs, the
+  // the endings are different and so if we try to give the last latter a haraka from the endings object
+  // below then itll be wrong. we need to put the ending for the last haraka in the actual template
+  // itself this time which is different from how we did it for the other verb types.
+  // now we dont need to do this for every baab, but only the ones that correspond to the 
+  // NAQIS_DROPPING_SLOTS_MUDARI, because if you take a look at the stems we have the extra haraka
+  // defined only on the dropping stems 
+  // the mabni seegahs dont need sukun for naqis (3fp, 2fp)
+  mudari_raf: {
+    '3ms': A(D, ''),            '3md': A(F, 'ا' + 'ن' + K),      '3mp': A('', 'و' + 'ن' + F),
+    '3fs': A(D, ''),            '3fd': A(F, 'ا' + 'ن' + K),      '3fp': A('', 'ن' + F),
+    '2ms': A(D, ''),            '2md': A(F, 'ا' + 'ن' + K),      '2mp': A('', 'و' + 'ن' + F),
+    '2fs': A('', 'ي' + 'ن' + F), '2fd': A(F, 'ا' + 'ن' + K),      '2fp': A('', 'ن' + F),
+    '1s':  A(D, ''),            '1p':  A(D, ''),
+  },
+  // ALSO SIDE NOTE LATER CHANGE, but right now the raf endings are here because of the dammah on the 3ms, 3fs
+  // etc, and we have the imperative code in the conjugator doing the nasb and jazm, we should remove the dammah
+  // from here and make it consistent and move all those endings to to the code, however, it also makes sense
+  // to separate them because rafa has the noons and the other 2 do not.
+};
 
 /** ism fāʿil / ism mafʿūl / maṣdar templates, per form. */
 export const DERIVED_NOUN_STEMS = {};
+
+
+
+// aight so for the ai, aa and ia baabs, the mabni forms are good, and the regular consturction will 
+// make them easily.
+
+// for the au baab, the 3rd fem pl would be fine with regular construction but the 2nd fem pl would not
+// be because the wow changes to a ya.
+
+
+// for 2fs, we need the baab templates to define the haraka before the ya nun.
